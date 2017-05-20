c1550df30f2780f8ae35923328e6294ed9975fb8
Kevin Zhang <kevinzhang3702@gmail.com>
2017-05-20 00:10:19 -0400

    some stuff done, broken move tho?

--- script.py
+++ script.py
@@ -26,13 +26,13 @@ def first_pass( commands ):
         if command[0] == "basename":
             returnVal['basename'] = command[1]
         elif command[0] == "frames":
-            returnVal['num_frames'] = command[2]
+            returnVal['num_frames'] = command[1]
         elif command[0] == "vary":
             if 'num_frames' not in returnVal.keys():
                 exit
-        if 'basename' not in returnVal.keys():
-            print "Basename not found, using default ( \"animation\" )"
-            returnVal['basename'] = 'animation'
+    if 'basename' not in returnVal.keys():
+        print "Basename not found, using default ( \"animation\" )"
+        returnVal['basename'] = 'animation'
     return returnVal
 
 
@@ -55,7 +55,7 @@ def first_pass( commands ):
   ===================="""
 def second_pass( commands, num_frames ):
     varyCommands = [ command for command in commands if command[0] == "vary" ]
-    knobNames = ( command[1] for command in varyCommands )
+    knobNames = set( command[1] for command in varyCommands )
 
     knobValues = { }
     for varyCommand in varyCommands:
@@ -65,11 +65,11 @@ def second_pass( commands, num_frames ):
             print "Vary Command out of range for %s"%( varyCommand[2] )
             exit
         frame = varyCommand[2]
-        finalFrame = varyCommand[3]
-        diff = ( frame - finalFrame ) / abs( frame - finalFrame )
-        knobChange = ( varyCommand[5] - varyCommand[4] ) / ( abs(frame - finalFrame) + 1 )
-        currentVal = varyCommand[4]
-        while( frame != finalFrame ):
+        finalFrame = varyCommand[3] 
+        diff = ( finalFrame - frame ) / abs( frame - finalFrame )
+        knobChange = ( varyCommand[5] - varyCommand[4] ) * 1.0 / ( abs(frame - finalFrame) + 1 )
+        currentVal = varyCommand[4] 
+        while( frame != finalFrame + 1):
             knobValues[ varyCommand[1] ][ frame ] = currentVal
             frame += diff
             currentVal += knobChange
@@ -99,20 +99,27 @@ def run(filename):
 
     
     #Animation Stuff
-    info = first_pass( commands ):
+    info = first_pass( commands)
     ANIMATION = True if 'num_frames' in info else False
     basename = info['basename']
     
     if ANIMATION:
-        num_frames = info['num_frames'] if ANIMATION else 1
+        num_frames = info['num_frames']
         knobInfo = second_pass( commands, num_frames )
         knobNames = knobInfo[0]
         knobValues = knobInfo[1]
         
         for frame in range( num_frames ):
-            savefile = "/anim/%s%s"%(basename, tres(frame) )
-            knobTable = { knobName: knobValue  for knobName in knobNames for  knobValue in [ knobValues[knobName][frame] ] }
-    
+            savefile = "./anim/%s%s.png"%(basename, tres(frame) )
+            knobTable = { knobName: knobValue  for knobName in knobNames for knobValue in [ knobValues[knobName][frame] ] }
+            print knobNames
+            exit
+            tmp = new_matrix()
+            ident(tmp)
+            stack = [ [x[:] for x in tmp] ]
+            screen = new_screen()
+            tmp = []
+            
             for command in commands:
                 print command
                 c = command[0]
@@ -181,67 +188,70 @@ def run(filename):
                     display(screen)
                 elif c == 'save':
                     save_extension(screen, args[0])
+
+            save_extension( screen, savefile )
+        make_animation( basename )
     else:
         for command in commands:
-        print command
-        c = command[0]
-        args = command[1:]
-
-        if c == 'box':
-            add_box(tmp,
-                    args[0], args[1], args[2],
-                    args[3], args[4], args[5])
-            matrix_mult( stack[-1], tmp )
-            draw_polygons(tmp, screen, color)
-            tmp = []
-        elif c == 'sphere':
-            add_sphere(tmp,
-                       args[0], args[1], args[2], args[3], step)
-            matrix_mult( stack[-1], tmp )
-            draw_polygons(tmp, screen, color)
-            tmp = []
-        elif c == 'torus':
-            add_torus(tmp,
-                      args[0], args[1], args[2], args[3], args[4], step)
-            matrix_mult( stack[-1], tmp )
-            draw_polygons(tmp, screen, color)
-            tmp = []
-        elif c == 'move':
-            tmp = make_translate(args[0], args[1], args[2])
-            matrix_mult(stack[-1], tmp)
-            stack[-1] = [x[:] for x in tmp]
-            tmp = []
-        elif c == 'scale':
-            tmp = make_scale(args[0], args[1], args[2])
-            matrix_mult(stack[-1], tmp)
-            stack[-1] = [x[:] for x in tmp]
-            tmp = []
-        elif c == 'rotate':
-            theta = args[1] * (math.pi/180)
-            if args[0] == 'x':
-                tmp = make_rotX(theta)
-            elif args[0] == 'y':
-                tmp = make_rotY(theta)
-            else:
-                tmp = make_rotZ(theta)
-            matrix_mult( stack[-1], tmp )
-            stack[-1] = [ x[:] for x in tmp]
-            tmp = []
-        elif c == 'push':
-            stack.append([x[:] for x in stack[-1]] )
-        elif c == 'pop':
-            stack.pop()
-        elif c == 'display':
-            display(screen)
-        elif c == 'save':
-            save_extension(screen, args[0])
+            print command
+            c = command[0]
+            args = command[1:]
+            
+            if c == 'box':
+                add_box(tmp,
+                        args[0], args[1], args[2],
+                        args[3], args[4], args[5])
+                matrix_mult( stack[-1], tmp )
+                draw_polygons(tmp, screen, color)
+                tmp = []
+            elif c == 'sphere':
+                add_sphere(tmp,
+                           args[0], args[1], args[2], args[3], step)
+                matrix_mult( stack[-1], tmp )
+                draw_polygons(tmp, screen, color)
+                tmp = []
+            elif c == 'torus':
+                add_torus(tmp,
+                          args[0], args[1], args[2], args[3], args[4], step)
+                matrix_mult( stack[-1], tmp )
+                draw_polygons(tmp, screen, color)
+                tmp = []
+            elif c == 'move':
+                tmp = make_translate(args[0], args[1], args[2])
+                matrix_mult(stack[-1], tmp)
+                stack[-1] = [x[:] for x in tmp]
+                tmp = []
+            elif c == 'scale':
+                tmp = make_scale(args[0], args[1], args[2])
+                matrix_mult(stack[-1], tmp)
+                stack[-1] = [x[:] for x in tmp]
+                tmp = []
+            elif c == 'rotate':
+                theta = args[1] * (math.pi/180)
+                if args[0] == 'x':
+                    tmp = make_rotX(theta)
+                elif args[0] == 'y':
+                    tmp = make_rotY(theta)
+                else:
+                    tmp = make_rotZ(theta)
+                matrix_mult( stack[-1], tmp )
+                stack[-1] = [ x[:] for x in tmp]
+                tmp = []
+            elif c == 'push':
+                stack.append([x[:] for x in stack[-1]] )
+            elif c == 'pop':
+                stack.pop()
+            elif c == 'display':
+                display(screen)
+            elif c == 'save':
+                save_extension(screen, args[0])
 
 def tres( num ):
     if num < 10:
-        return "00%d"%(num)
+        return "00%s"%(num)
     if num < 100:
-        return "0%d"%(num)
+        return "0%s"%(num)
     if num < 1000:
-        return "%d"%(num)
+        return "%s"%(num)
     print( "Frame Size too large" )
     return ""
